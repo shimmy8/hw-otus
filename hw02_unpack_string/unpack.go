a3bce5be360b7821fa910ccafa74d8ff8443671a
Maxim Gromov <gromov.91@gmail.com>
2023-06-14 13:43:51 +0300

    Hw02 unpack string (#2)
    * hw02 complete
    
    * fix lint
    
    * review fixes
    
    ---------
    
    Co-authored-by: Maxim Gromov <gromov_ma@magnit.ru>

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,36 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(inp string) (string, error) {
+	var prevStr string
+	var outpBuilder strings.Builder
+
+	for _, val := range inp {
+		if '0' <= val && val <= '9' { // current rune is a number
+			if prevStr == "" || ("0" <= prevStr && prevStr <= "9") { // prev string is empty or a number
+				return "", ErrInvalidString
+			}
+
+			repeats, _ := strconv.Atoi(string(val))
+			if repeats > 0 { // write only repeats > 0
+				outpBuilder.WriteString(strings.Repeat(prevStr, repeats))
+			}
+		} else if "0" > prevStr || prevStr > "9" { // current rune and prevStr are not numbers
+			outpBuilder.WriteString(prevStr)
+		}
+
+		prevStr = string(val)
+	}
+
+	if "0" > prevStr || prevStr > "9" { // write last symbol if not a number
+		outpBuilder.WriteString(prevStr)
+	}
+
+	return outpBuilder.String(), nil
 }
