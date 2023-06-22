25c854e458affdf6f4a59c24c716642c919bd600
Maxim Gromov <gromov.91@gmail.com>
2023-06-22 10:47:11 +0300

    hw 03 complete (#3)
    * hw 03 complete
    
    * Review fixes
    
    ---------
    
    Co-authored-by: Maxim Gromov <gromov_ma@magnit.ru>

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,59 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+	"unicode"
+)
+
+type WordsCount struct {
+	word string
+	cnt  uint
+}
+
+func cleanWord(word string) string {
+	// set word to lowercase and trim symbols
+	word = strings.ToLower(word)
+	return strings.TrimFunc(word, func(r rune) bool {
+		return !unicode.IsLetter(r) && !unicode.IsNumber(r)
+	})
+}
+
+func Top10(input string) []string {
+	const Limit = 10
+
+	wordsCntMap := make(map[string]uint)
+	// count words
+	for _, word := range strings.Fields(input) {
+		word = cleanWord(word)
+		if word == "" {
+			// skip words containing only symbols
+			continue
+		}
+		wordsCntMap[word]++
+	}
+
+	// store counts to a slice of structs for further sorting
+	wordsCntStructs := make([]WordsCount, 0, len(wordsCntMap))
+	for word, cnt := range wordsCntMap {
+		wordsCntStructs = append(wordsCntStructs, WordsCount{word, cnt})
+	}
+	// sort by cnt DESC and word ASC
+	sort.Slice(wordsCntStructs, func(i, j int) bool {
+		if wordsCntStructs[i].cnt != wordsCntStructs[j].cnt {
+			return wordsCntStructs[i].cnt > wordsCntStructs[j].cnt
+		}
+		return wordsCntStructs[i].word < wordsCntStructs[j].word
+	})
+
+	// store top words to the result slice
+	result := make([]string, 0, Limit)
+	for idx, wordsCnt := range wordsCntStructs {
+		result = append(result, wordsCnt.word)
+		if idx == Limit-1 {
+			break
+		}
+	}
+
+	return result
 }
