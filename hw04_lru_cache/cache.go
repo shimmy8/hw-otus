732392538103793e079a61b0f9dcfe5aab3a886a
Maxim Gromov <gromov_ma@magnit.ru>
2023-07-10 11:44:11 +0300

    hw04 done

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,11 +9,39 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+}
+
+func (cache lruCache) Set(key Key, i interface{}) bool {
+	cachedElem, inCache := cache.items[key]
+
+	if inCache {
+		cachedElem.Value = i
+		cache.queue.PushFront(cachedElem)
+	} else {
+		if cache.capacity == cache.queue.Len() {
+			cache.queue.Remove(cache.queue.Back())
+		}
+		elem := cache.queue.PushFront(i)
+		cache.items[key] = elem
+	}
+	return inCache
+}
+
+func (cache lruCache) Get(key Key) (interface{}, bool) {
+	cachedElem, inCache := cache.items[key]
+	if inCache {
+		cache.queue.MoveToFront(cachedElem)
+		return cachedElem.Value, inCache
+	}
+	return nil, inCache
+}
+
+func (cache *lruCache) Clear() {
+	cache.queue = NewList()
+	cache.items = make(map[Key]*ListItem)
 }
 
 func NewCache(capacity int) Cache {
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,8 +17,83 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	Length    int
+	FirstItem *ListItem
+	LastItem  *ListItem
+}
+
+func (lst list) Len() int {
+	return lst.Length
+}
+
+func (lst list) Front() *ListItem {
+	return lst.FirstItem
+}
+
+func (lst list) Back() *ListItem {
+	return lst.LastItem
+}
+
+func (lst *list) PushFront(v interface{}) *ListItem {
+	listItem := ListItem{Value: v}
+	if lst.FirstItem == nil {
+		lst.FirstItem = &listItem
+		lst.LastItem = &listItem
+	} else {
+		listItem.Next = lst.FirstItem
+		lst.FirstItem.Prev = &listItem
+		lst.FirstItem = &listItem
+	}
+	lst.Length++
+	return &listItem
+}
+
+func (lst *list) PushBack(v interface{}) *ListItem {
+	listItem := ListItem{Value: v}
+	if lst.LastItem == nil { // lst is empty
+		lst.LastItem = &listItem
+		lst.FirstItem = &listItem
+	} else {
+		listItem.Prev = lst.LastItem
+		lst.LastItem.Next = &listItem
+		lst.LastItem = &listItem
+	}
+	lst.Length++
+	return &listItem
+}
+
+func (lst *list) Remove(i *ListItem) {
+	if i.Prev == nil {
+		lst.FirstItem = i.Next
+	} else {
+		i.Prev.Next = i.Next
+	}
+
+	if i.Next == nil {
+		lst.LastItem = i.Prev
+	} else {
+		i.Next.Prev = i.Prev
+	}
+
+	lst.Length--
+	i = nil
+}
+
+func (lst *list) MoveToFront(i *ListItem) {
+	if lst.FirstItem == i {
+		return
+	}
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	}
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	}
+
+	lst.FirstItem.Prev = i
+	i.Next = lst.FirstItem
+	i.Prev = nil
+	lst.FirstItem = i
 }
 
 func NewList() List {
