d231f99d41016348d70a882a51dc195e0e97caeb
Maxim Gromov <gromov_ma@magnit.ru>
2023-07-25 09:47:13 +0300

    review changes

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -10,22 +10,21 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 type Task func() error
 
 type errorCounter struct {
-	mu        sync.Mutex
+	mu        sync.RWMutex
 	val       int
 	threshold int
 }
 
 func (counter *errorCounter) inc() {
+	defer counter.mu.Unlock()
 	counter.mu.Lock()
 	counter.val++
-	counter.mu.Unlock()
 }
 
 func (counter *errorCounter) reachedThreshold() bool {
-	counter.mu.Lock()
-	thReached := counter.val >= counter.threshold
-	counter.mu.Unlock()
-	return thReached
+	defer counter.mu.RUnlock()
+	counter.mu.RLock()
+	return counter.val >= counter.threshold
 }
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
@@ -36,7 +35,6 @@ func Run(tasks []Task, n, m int) error {
 	ch := make(chan int)
 	errorCnt := errorCounter{
 		threshold: m,
-		val:       0,
 	}
 
 	for i := 0; i < n; i++ {
