3c50536ea038e4896611a383cd288c71025af92e
Maxim Gromov <gromov_ma@magnit.ru>
2023-08-07 11:21:29 +0300

    hw 06 complete

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,30 @@ type (
 
 type Stage func(in In) (out Out)
 
+func ExecuteStage(in In, done In, stage Stage) Out {
+	stageIn := make(Bi)
+	go func() {
+		defer close(stageIn)
+		for n := range in {
+			select {
+			case <-done:
+				return
+			default:
+				stageIn <- n
+			}
+		}
+	}()
+	stageOut := stage(stageIn)
+	return stageOut
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := make(Out)
+
+	for _, stage := range stages {
+		out = ExecuteStage(in, done, stage)
+		in = out
+	}
+
+	return out
 }
